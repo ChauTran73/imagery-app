29beafd27ad8d54510b2a800087160508210ebea
Chau Tran <cmtran7393@gmail.com>
2019-11-14 01:52:09 -0500

    add delete icon

--- src/components/MyPins/MyPins.js
+++ src/components/MyPins/MyPins.js
@@ -3,14 +3,18 @@ import ImageListItem from '../../components/ImageListItem/ImageListItem'
 import ImageApiService from '../../services/image-api-service'
 import { Button } from '../Utils/Utils'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
 
 class MyPins extends Component {
     renderImages(){
-        return this.props.myPins.map(image =>
-          <ImageListItem
+        return this.props.myPins.map(image => 
+        <div>
+         <ImageListItem
             key={image.id}
             image={image}
           />
+          <FontAwesomeIcon icon={faTrash} size="lg" />
+         </div>
         )
       }
     render() {
