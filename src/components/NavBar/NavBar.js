057e900384f8d866aed58b3063126c4554a181a8
Chau Tran <cmtran7393@gmail.com>
2020-01-14 22:52:17 -0500

    fixed rendering login

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,43 +3,28 @@ import { Link } from 'react-router-dom';
 import './NavBar.css';
 import TokenService from '../../services/token-service'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faImages } from "@fortawesome/free-solid-svg-icons"
 import { faSearch } from "@fortawesome/free-solid-svg-icons"
 import UserService from '../../services/user-service';
 
 class NavBar extends Component {
-    //navbar only re-renders when there is a state change
-    //login success => save token and user in local storage -> setState isLoggedin true, hasUser true
-    constructor(props) {
-        super(props);
-        this.state = { //async
-            isLoggedIn: TokenService.hasAuthToken(),
-            hasUser: UserService.hasUser(),
-        }
-               
-    }
-   
     handleLogoutClick = () => {
         TokenService.clearAuthToken();
-        UserService.clearUser();
-        this.setState({isLoggedIn: false, hasUser: false})
+        UserService.clearUser();     
     }
-    
     renderLogoutLink() { //when the user has already logged in
-        
         return (
             <div className='Navbar_logged-in'>
                 <Link to='/my-wall' >
                     My Wall
                 </Link>
-                {'     '}
+                
                 <Link
                     onClick={this.handleLogoutClick}
                     to='/'>
                     Logout
                 </Link>
-                {'      '}
-                <Link to= 'my-wall'>
+                
+                <Link to='my-wall'>
                     Logged in as {UserService.getUser()}
                 </Link>
             </div>
@@ -51,7 +36,7 @@ class NavBar extends Component {
                 <Link to='/login' >
                     Login
                 </Link>
-            {'    '}
+                {/* {'    '} */}
                 <Link to='/register'>
                     Register
                 </Link>
@@ -64,24 +49,24 @@ class NavBar extends Component {
             <nav className='navbar'>
                 <h1>
                     <Link to='/'>
-                       <FontAwesomeIcon icon={faImages} size='lg' className='blue'/>
-                        {' '}
                         Imagery
                     </Link>
                 </h1>
                 <div className="searchbox">
                     <input type="text" name="searchBar" placeholder="Search for an image" />
-                    <span className="search__icon"> <FontAwesomeIcon icon={faSearch} /></span>
+                    <span className="search__icon"> <FontAwesomeIcon icon={faSearch} size='lg'/></span>
                 </div>
-                <Link to="/">
-                    Home
-                </Link>
+                <div className='main-nav'>
 
-                {this.state.isLoggedIn && this.state.hasUser
-                    ? this.renderLogoutLink()
-                    : this.renderLoginLink() 
-                }
+                    <Link to="/">
+                        Home
+                    </Link>
+                    {TokenService.hasAuthToken()
+                        ? this.renderLogoutLink()
+                        : this.renderLoginLink()
+                    }
 
+                </div>
             </nav>
         );
     }
