164da672d42f06914577e0701ce702b1de51adc4
Chau Tran <cmtran7393@gmail.com>
2020-01-26 08:48:40 -0500

    save and delete feature

--- src/components/ImageListItem/ImageListItem.js
+++ src/components/ImageListItem/ImageListItem.js
@@ -1,32 +1,45 @@
 import React, { Component } from 'react';
 import './ImageListItem.css';
 import { Link } from 'react-router-dom'
-import { Button, Input } from '../Utils/Utils'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from "@fortawesome/free-solid-svg-icons"
-
+import { faHeart , faTrash} from "@fortawesome/free-solid-svg-icons"
+import ImageListContext from '../../contexts/ImageListContext'
 
 class ImageListItem extends Component {
+    static contextType = ImageListContext;
 
     render() {
         const { image } = this.props
         return (
-            <Link to={`/images/${image.id}`} className='ImageListItem'>
-                <div className='ImageListItem__image' >
-                    <img src={`${image.url}`} />
-
-                    <div className='ImageListItem_details'>
-                        <div className='ImageListItem_text'>
-                            <h2 className='ImageListItem_heading'>{image.title}</h2>
-                            <span >
-                                <FontAwesomeIcon icon={faHeart} className="pin" size="lg" />
-                            </span>
-
+            <div className='image-container'>
+                <Link to={`/images/${image.id}`} className='ImageListItem'>
+                    <div className='ImageListItem__image' >
+                        <img src={`${image.url}`} />
+                       
+                        <div className='ImageListItem_details'>
+                            <div className='ImageListItem_text'>
+                                <h2 className='ImageListItem_heading'>{image.title}</h2>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </Link>
 
+                    </div>
+                </Link>
+                        {       
+                            this.props.renderSaveButton ?
+                                <span className='save-image'
+                                    onClick={() => this.props.handleSaveImage(image)}>
+                                    <FontAwesomeIcon icon={faHeart} size='lg'/>
+                                </span> : null
+                        }
+                        {
+                            this.props.renderSaveButton && localStorage.getItem('user_id') == image.author.id ? 
+                            <div onClick={() => this.props.handleDeleteImage(image.id)}>
+                                <FontAwesomeIcon icon={faTrash}/> 
+                            </div>: null
+        
+                        }
+                    
+            </div>
         );
     }
 }
