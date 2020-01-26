b4ebb2313e3aec7d2cee16d8ca1a7af6edbc5e02
Chau Tran <cmtran7393@gmail.com>
2020-01-26 08:49:42 -0500

    passed down user id

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -27,7 +27,7 @@ class LoginForm extends Component {
                 username.value = ''
                 password.value = ''
                 TokenService.saveAuthToken(res.authToken)
-                UserService.saveUser(res.sub)
+                UserService.saveUser(res.sub, res.payload.user_id)
                 this.props.onLoginSuccess() //redirect to the homepage
             })
             .catch(res => {
