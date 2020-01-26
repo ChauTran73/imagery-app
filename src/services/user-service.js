c382c259ebb9b3a75517f4f027974385b86ad0e1
Chau Tran <cmtran7393@gmail.com>
2020-01-26 08:52:18 -0500

    passed down user id

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,14 +1,16 @@
 import config from '../config'
 
 const UserService = {
-    saveUser(user) {
-        window.localStorage.setItem(config.USER_NAME, user)
+    saveUser(user, userId) {
+        window.localStorage.setItem(config.USER_NAME_IMAGERY, user)
+        window.localStorage.setItem(config.USER_ID_IMAGERY, userId)
     },
     getUser(){
-        return window.localStorage.getItem(config.USER_NAME)
+        return window.localStorage.getItem(config.USER_NAME_IMAGERY, config.USER_ID_IMAGERY)
     },
     clearUser(){
-        window.localStorage.removeItem(config.USER_NAME)
+        window.localStorage.removeItem(config.USER_NAME_IMAGERY)
+        window.localStorage.removeItem(config.USER_ID_IMAGERY)
     },
     hasUser() {
         return !!this.getUser()
