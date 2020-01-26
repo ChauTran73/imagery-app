e64e41477d9e46b3279e988e91b690981f6891d4
Chau Tran <cmtran7393@gmail.com>
2020-01-26 08:47:50 -0500

    wrapped context around App

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './App';
+import {ImageListProvider} from './contexts/ImageListContext'
 import './index.css';
 
 
 ReactDOM.render(
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>,
-    document.getElementById('root')
-  )
+  <BrowserRouter>
+    <ImageListProvider>
+      <App />
+    </ImageListProvider>
+
+  </BrowserRouter>,
+  document.getElementById('root')
+)
