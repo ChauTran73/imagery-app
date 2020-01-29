efb5b0127fab986c69de7e404c8d51d18e3e68cd
Chau Tran <cmtran7393@gmail.com>
2020-01-29 11:11:40 -0500

    fixed naming convention

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ export default {
      API_ENDPOINT: 'https://calm-ravine-48962.herokuapp.com/api',
     // API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: 'imagery-client-auth-token',
-    USER_NAME_IMAGERY: 'user_name',
-    USER_ID_IMAGERY: 'user_id'
+    USER_NAME: 'user_name',
+    USER_ID: 'user_id'
 }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,15 +2,15 @@ import config from '../config'
 
 const UserService = {
     saveUser(user, userId) {
-        window.localStorage.setItem(config.USER_NAME_IMAGERY, user)
-        window.localStorage.setItem(config.USER_ID_IMAGERY, userId)
+        window.localStorage.setItem(config.USER_NAME, user)
+        window.localStorage.setItem(config.USER_ID, userId)
     },
     getUser(){
-        return window.localStorage.getItem(config.USER_NAME_IMAGERY, config.USER_ID_IMAGERY)
+        return window.localStorage.getItem(config.USER_NAME, config.USER_ID)
     },
     clearUser(){
-        window.localStorage.removeItem(config.USER_NAME_IMAGERY)
-        window.localStorage.removeItem(config.USER_ID_IMAGERY)
+        window.localStorage.removeItem(config.USER_NAME)
+        window.localStorage.removeItem(config.USER_ID)
     },
     hasUser() {
         return !!this.getUser()
