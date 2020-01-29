8a263128966ad15d0bf62dfe27587024952ba152
Chau Tran <cmtran7393@gmail.com>
2020-01-29 10:33:27 -0500

    added userId

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export default {
-    API_ENDPOINT: 'https://calm-ravine-48962.herokuapp.com/api',
+     API_ENDPOINT: 'https://calm-ravine-48962.herokuapp.com/api',
+    // API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: 'imagery-client-auth-token',
-    USER_NAME: 'user-name'
+    USER_NAME_IMAGERY: 'user_name',
+    USER_ID_IMAGERY: 'user_id'
 }
